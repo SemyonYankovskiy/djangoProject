eed4f65dd6b0f4a0fb3f8ccc51b1c026ac0d6083
syankovskiy <semyon21@mail.ru>
2023-09-28 12:25:34 +0300

    learn forms and database

--- hello/views.py
+++ hello/views.py
@@ -1,3 +1,4 @@
+from django.core.exceptions import ObjectDoesNotExist, MultipleObjectsReturned
 from django.http import HttpResponse, HttpResponseNotFound, HttpResponseForbidden, HttpResponseBadRequest
 from django.shortcuts import render
 
@@ -25,18 +26,39 @@ def postuser(request):
             langs = userform.cleaned_data["languages"]
             dest = userform.cleaned_data["destiny"]
             slide = userform.cleaned_data["slider"]
+
             # СОхраненеи в базу
             name_bd = Person.objects.create(name=name, age=age, languages=langs, destiny=dest, slider=slide)
 
-
-
-
+            #Передача в result_test.html
             data = {"langs_test": langs, "dest_test": dest, "slide": slide, "NAME": name, "AGE": age}
-            return render(request, "result_test.html", context=data)
+            return render(request, "result_send_form.html", context=data)
 
 
         return render(request, "new_page.html", {"form": userform})
 
     return render(request, "new_page.html", {"form": userform})
 
+def get_from_base(request):
+    user_name = request.GET.get("my-name", "")  # Из запроса
+
+    try:
+        person = Person.objects.filter(name=user_name)
+        data = {"NAME_1": person.name}
+        return render(request, "result_test.html", context=data)
+    except ObjectDoesNotExist:
+        error = "Нет такого имени"
+        err = {"error":error}
+        return render(request, "result_test.html", err)
+    except MultipleObjectsReturned:
+        error = "Найдено более одного объекта"
+        err = {"error": error}
+        return render(request, "result_test.html", err)
+
+def delete_from_base(request):
+    Person.objects.all().delete()
+    delete_answer = "База данных удалена"
+    userform = UserForm()
+    answer = {"answer": delete_answer, "form": userform}
+    return render(request, "new_page.html", answer)
 
